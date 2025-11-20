d8f162f533e7b852aeb56c54704be76ec1bb60ee
Ced <98132233+zMeliodas@users.noreply.github.com>
2025-11-20 18:43:02 +0800

    Deploy config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
+const repoName = '/todo-app/'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   daisyui: {
     themes: true,
   },
+  base: repoName
 });
